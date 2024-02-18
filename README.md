c27eee9f592c7111326bc62c9cc51394f7a166fa
Xiaokui Shu <subbyte@gmail.com>
2024-02-18 14:48:54 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# cloudflare-ddns-posix
+# Cloudflare Dynamic DNS Update Service
+
+- POSIX-compliant
+- Cloudflare API v4
+- Bearer authentication
+- DNS proxy recognition
+- Installed as a cron job
+- Compatible with OpenBSD
+
+## Usage
+
+```console
+$ ./install.sh
+```
+
+## Requirement
+
+- `curl`
+- `jq`
+
+## Design Consideration
+
+No Python. Python 3.10 drops support for LibreSSL, thus not working on OpenBSD.
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+echo "Welcome to install the Cloudflare DDNS service."
+echo -n "  Enter zone id: "
+read ZONE_ID
+echo -n "  Enter API token: "
+read TOKEN
+
+# GNU whereis is different from BSD whereis
+WHERETEST=$(whereis sh | grep ":")
+if [ -n "$WHERETEST" ]
+then
+    SH_PATH=$(whereis sh | awk '{print $2}')
+else
+    SH_PATH=$(whereis sh)
+fi
+
+echo "#!$SH_PATH" > ddns.sh
+echo "" >> ddns.sh
+echo "ZONE_ID=$ZONE_ID" >> ddns.sh
+echo "CLOUDFLARE_API_TOKEN=$TOKEN" >> ddns.sh
+echo "" >> ddns.sh
+cat payload.sh >> ddns.sh
+
+SERVICE_PATH=$(realpath $(dirname $0))/ddns.sh
+
+chmod 755 $SERVICE_PATH
+
+echo "Install service as cron job..."
+crontab -l | { cat; echo "*/10 * * * * $SERVICE_PATH"; } | crontab -
+
+echo "Completed."
